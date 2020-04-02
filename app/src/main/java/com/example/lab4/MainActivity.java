b970e101783ceacc6436132d36f536580bb02a02
NickHemingwayy <NickHemingwayy@users.noreply.github.com>
2020-04-01 22:42:01 -0700

    here is the account creation/sign in

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -5,24 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoutBtn = findViewById(R.id.logoutBtn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(getApplicationContext(),login.class));
+            }
+        });
+
     }
 
-    public void Read(View view) {
-        Intent intent = new Intent(this, Read.class);
-        startActivity(intent);
 
-
-    }
-
-    public void write(View view) {
-        Intent intent = new Intent(this, Write.class);
-        startActivity(intent);
-    }
 }
